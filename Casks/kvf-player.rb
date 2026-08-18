7cd4a0a66dd16e37b5e84a80efd3b76ed625733b
steingmo <steingrimosa@gmail.com>
2026-08-18 20:01:09 +0100

    kvf-player 1.1.0

--- Casks/kvf-player.rb
+++ Casks/kvf-player.rb
@@ -1,6 +1,6 @@
 cask "kvf-player" do
-  version "1.0.0"
-  sha256 "566dcfc2b8bc92154efedf6a2958857dd0d9df5a62a1de78a6aa5cb7ed2caae7"
+  version "1.1.0"
+  sha256 "0ad0b1a75ddf296c8c7311b12de788cdb61e124a294039965d7e1c3ec7b6a4d1"
 
   url "https://github.com/steingmo/kvf-player/releases/download/v#{version}/KVF.zip"
   name "KVF"
@@ -12,6 +12,7 @@ cask "kvf-player" do
     strategy :github_latest
   end
 
+  auto_updates true
   depends_on macos: :sonoma
 
   app "KVF.app"
